c913828e1f76bda34f141a31616373a067207f82
Dolphin Wood <dolphin.w.e@gmail.com>
2021-10-08 20:46:59 +0900

    fix(RootDialog): remove unnecessary `show` prop

--- src/components/RootDialog.svelte
+++ src/components/RootDialog.svelte
@@ -1,25 +1,12 @@
 <script lang="ts">
   import Portal from 'svelte-portal';
   import Dialog from '@smui/dialog/styled';
-  import { createEventDispatcher } from 'svelte';
 
   export let open: boolean;
-
-  let show = open;
-  $: show = open || show;
-
-  const dispatch = createEventDispatcher();
-
-  function onClose() {
-    show = false;
-    dispatch('closed');
-  }
 </script>
 
-{#if show}
-  <Portal target="body">
-    <Dialog bind:open on:MDCDialog:closed={onClose} {...$$restProps}>
-      <slot></slot>
-    </Dialog>
-  </Portal>
-{/if}
+<Portal target="body">
+  <Dialog bind:open on:MDCDialog:closed {...$$restProps}>
+    <slot></slot>
+  </Dialog>
+</Portal>
