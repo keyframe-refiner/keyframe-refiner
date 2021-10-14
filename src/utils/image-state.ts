7e7ec4b2cfb5fae28ec47483142eede29d0d7272
Dolphin Wood <dolphin.w.e@gmail.com>
2021-10-15 02:32:44 +0900

    feat(utils): add getImageState() helper function

---
+++ src/utils/image-state.ts
@@ -0,0 +1,46 @@
+import type { ImageCanvas } from '../image-canvas';
+import { STEP } from '../step';
+
+export type ImageState = {
+  image?: ImageCanvas,
+  dimmed?: boolean,
+  processing?: boolean,
+  error?: string,
+};
+
+export function getImageState(
+  input: ImageCanvas | undefined,
+  output: ImageCanvas | Error | undefined,
+  refImage: ImageCanvas | undefined,
+  currentStep: STEP,
+): ImageState {
+  let image: ImageCanvas | undefined;
+  let dimmed: boolean | undefined;
+  let processing: boolean | undefined;
+  let error: string | undefined;
+
+  if (currentStep === STEP.RUN_CV) {
+    if (!output) {
+      image = input;
+      processing = true;
+      dimmed = true;
+    } else if (output instanceof Error) {
+      image = input;
+      error = output.message;
+      dimmed = true;
+    } else {
+      image = output;
+      dimmed = false;
+    }
+  } else {
+    image = input;
+    dimmed = !!refImage && image !== refImage;
+  }
+
+  return {
+    image,
+    error,
+    dimmed,
+    processing,
+  };
+}
