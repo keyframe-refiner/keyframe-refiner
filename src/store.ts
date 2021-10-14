d6f309deb1ded1040f0180e77bbdfacfa53c371b
Dolphin Wood <dolphin.w.e@gmail.com>
2021-10-15 03:39:56 +0900

    refactor: rename stores

--- src/store.ts
+++ src/store.ts
@@ -4,13 +4,22 @@ import { defaultSteps } from './step';
 import { StepManager } from './utils/step-manager';
 import { CVWorker } from './cv-worker';
 
-import { writable } from 'svelte/store';
+import { writable, derived } from 'svelte/store';
 import type { ImageCanvas } from './image-canvas';
 
 export const inputList = writable(List<ImageCanvas>());
 export const outputList = writable(List<ImageCanvas | Error>());
 
-export const selectedImage = writable<ImageCanvas | undefined>();
+export const selectedIndex = writable(0);
+export const selectedInput = derived(
+  [inputList, selectedIndex],
+  ([$inputList, $selectedIndex]) => $inputList.get($selectedIndex),
+);
+export const selectedOutput = derived(
+  [outputList, selectedIndex],
+  ([$outputList, $selectedIndex]) => $outputList.get($selectedIndex),
+);
+
 export const refImage = writable<ImageCanvas | undefined>();
 
 export const pivotPoint = writable(new Point());
@@ -33,4 +42,4 @@ export const calculatingPivot = writable(false);
 export const showROI = writable(false);
 export const showPivot = writable(false);
 export const showRefImage = writable(false);
-export const cvRunning = writable(false);
+export const showInputImage = writable(false);
