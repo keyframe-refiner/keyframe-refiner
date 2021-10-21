604f62f1454ad8abcdd012d02cd1e2b31856b0f4
Dolphin Wood <dolphin.w.e@gmail.com>
2021-10-21 23:48:49 +0900

    refactor(CVRunner): use floating download button

--- src/views/property-drawers/CVRunner.svelte
+++ src/views/property-drawers/CVRunner.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
+  import pAll from 'p-all';
   import { ZipWriter, BlobWriter, BlobReader } from '@zip.js/zip.js';
   import { hasContext, getContext } from 'svelte';
-  import Button from '@smui/button/styled';
-  import { fade } from 'svelte/transition';
   import type Scrollbar from 'smooth-scrollbar';
+  import { mdiDownload } from '@mdi/js';
+  import Portal from 'svelte-portal';
+  import Fab from '@smui/fab/styled';
+  import Button from '@smui/button/styled';
+  import CircularProgress from '@smui/circular-progress/styled';
 
   import Drawer from '../../components/Drawer.svelte';
+  import SVGIcon from '../../components/SVGIcon.svelte';
   import { ImageCanvas } from '../../utils/image-canvas';
   import { STEP } from '../../step';
   import {
@@ -61,14 +66,21 @@
 
     const zipWriter = new ZipWriter(new BlobWriter('application/zip'));
 
-    for (const res of $outputList) {
-      if (res instanceof ImageCanvas) {
-        await zipWriter.add(
-          res.filename,
-          new BlobReader(await res.toBlob()),
-        );
-      }
-    }
+    const actions = [...$outputList]
+      .filter(res => res instanceof ImageCanvas)
+      .map((image: ImageCanvas) => async () => zipWriter.add(
+        image.filename,
+        new BlobReader(await image.toBlob()),
+        { bufferedWrite: true },
+      ));
+
+    // HACK: image.toBlob() is a time-consuming task and will freeze the UI for seconds,
+    // so wait for UI updating here...
+    await new Promise(resolve => setTimeout(resolve));
+
+    await pAll(actions, {
+      concurrency: navigator.hardwareConcurrency || 4,
+    });
 
     const link = document.createElement('a');
     link.href = URL.createObjectURL(await zipWriter.close());
@@ -80,7 +92,9 @@
 </script>
 
 <Drawer highlight={$currentStep === targetStep}>
-  <span slot="title">OpenCV 処理</span>
+  <span slot="title">
+    OpenCV 処理
+  </span>
 
   <div slot="body">
     <Button
@@ -89,42 +103,80 @@
       disabled={$currentStep !== targetStep || cvRunning || progress === 1}
       on:click={runCV}
     >
-      {cvRunning ? '処理中...' : 'CV 処理開始'}
-    </Button>
-
-    <Button
-      variant="unelevated"
-      class="drawer-button download"
-      style={`--progress: ${progress * 100}%`}
-      disabled={progress !== 1}
-      on:click={download}
-    >
-      {creatingZip ? 'zip ファイル生成中...' : 'ダウンロード'}
-      {#if cvRunning}
-        <span class="progress" in:fade out:fade>{Math.floor(progress * 100)}%</span>
-      {/if}
+      {cvRunning ? '処理中...' : (progress === 1 ? 'CV 処理完了' : 'CV 処理開始')}
     </Button>
   </div>
 </Drawer>
 
+{#if cvRunning || progress === 1}
+  <Portal target="body">
+    <div class="download-btn" style={`--progress: ${progress * 100}%`}>
+      <Fab disabled={progress !== 1 || creatingZip} on:click={download} title="zip ファイルをダウンロード">
+        <SVGIcon icon={mdiDownload} />
+      </Fab>
+
+      {#if progress !== 1}
+        <CircularProgress class="progress" {progress} />
+      {/if}
+
+      {#if creatingZip}
+        <CircularProgress class="progress four-colors" indeterminate fourColor />
+      {/if}
+    </div>
+  </Portal>
+{/if}
+
 <style lang="scss">
-  :global(.download) {
-    position: relative;
-    overflow: hidden;
+  @use '@material/circular-progress/index' as circular-progress;
+  @use '@material/theme/index' as theme;
+
+  @keyframes zoomIn {
+    from {
+      transform: scale3d(0, 0, 0);
+    }
+
+    to {
+      transform: scale3d(1, 1, 1);
+    }
   }
 
-  .progress {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: var(--progress);
-    height: 100%;
-    color: var(--mdc-theme-surface);
-    background-color: var(--mdc-theme-primary);
-    font-size: 12px;
-    overflow: hidden;
+  .download-btn {
+    position: fixed;
+    right: 20px;
+    bottom: 20px;
+    transition: all .3s;
+    animation: zoomIn .3s easeOutBack;
+
+    :global {
+      button {
+        background: theme.$green-500;
+
+        &:disabled {
+          background: var(--placeholder);
+          cursor: not-allowed;
+          pointer-events: none;
+        }
+      }
+
+      .progress {
+        $progress-size: 68px;
+
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        width: $progress-size;
+        height: $progress-size;
+        margin-top: $progress-size / -2;
+        margin-left: $progress-size / -2;
+
+        @include circular-progress.color(theme.$green-500);
+
+        &.four-colors {
+            @include circular-progress.indeterminate-colors(
+              theme.$green-500 theme.$yellow-500 theme.$red-500 theme.$blue-500
+            );
+        }
+      }
+    }
   }
 </style>
