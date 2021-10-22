f2ce0462d4b2697834e1f886450bc0196688e973
Dolphin Wood <dolphin.w.e@gmail.com>
2021-10-22 23:29:18 +0900

    refactor(VariableTracker): automatically update tracker

--- src/utils/variable-tracker.ts
+++ src/utils/variable-tracker.ts
@@ -1,12 +1,20 @@
+import { afterUpdate } from 'svelte';
+
 export class VariableTracker {
-  #lastVars: any[] | undefined;
-  #varGetter: () => any[];
+  readonly #varGetter: () => any[];
+  #lastVars?: any[];
+
+  #changed: boolean = false;
 
   constructor(varGetter: () => any[]) {
     this.#varGetter = varGetter;
+
+    afterUpdate(() => {
+      this.#changed = this.#check();
+    });
   }
 
-  stale() {
+  #check() {
     if (!this.#lastVars) {
       this.#lastVars = this.#varGetter();
       return true;
@@ -18,5 +26,9 @@ export class VariableTracker {
     this.#lastVars = vars;
 
     return result;
+  }
+
+  stale() {
+    return this.#changed;
   }
 }
